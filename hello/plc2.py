227025194bf842c9d81619233c9aa3aedd40882c
unknown <ktu84562@naver.com>
2023-09-01 17:53:03 +0900

    day 2

--- hello/plc2.py
+++ hello/plc2.py
@@ -1,5 +1,4 @@
 import paho.mqtt.client as mqtt
-import json
 
 
 def on_connect(client, userdata, flags, rc):
@@ -13,6 +12,10 @@ def on_disconnect(client, userdata, flags, rc=0):
     print(str(rc))
 
 
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("subscribed: " + str(mid) + " " + str(granted_qos))
+
+
 def on_message(client, userdata, msg):
     data = msg.payload.decode("utf-8")
     print(str(msg.payload.decode("utf-8")))
@@ -20,16 +23,14 @@ def on_message(client, userdata, msg):
 
 # 새로운 클라이언트 생성
 client = mqtt.Client()
-# 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료), on_message(발행된 메세지가 들어왔을 때)
+# 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료), on_subscribe(topic 구독),
+# on_message(발행된 메세지가 들어왔을 때)
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
+client.on_subscribe = on_subscribe
 client.on_message = on_message
 # address : localhost, port: 1883 에 연결
 client.connect("localhost", 1883)
-
-# 메시지를 JSON 형식으로 만듭니다.
-message = {"tagId": "11", "value": "1"}
-# JSON 메시지를 문자열로 변환하여 발행합니다.
-client.publish("edukit/control", json.dumps(message), qos=1)
-
+# common topic 으로 메세지 발행
+client.subscribe("edukit/robotarm", 1)
 client.loop_forever()
