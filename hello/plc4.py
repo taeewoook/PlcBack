c337e87b8f1fa5e9bd22fabd9c20454972708644
jibinlim <130441571+jibinlim@users.noreply.github.com>
2023-09-13 14:40:07 +0900

    Update plc4.py

--- hello/plc4.py
+++ hello/plc4.py
@@ -28,10 +28,21 @@ trackflag = True
 
 
 def on_message(client, userdata, msg):
+    today = datetime.today().strftime("%Y-%m-%d")
     global trackflag
     data = msg.payload.decode("utf-8")
-    # print(str(msg.payload.decode("utf-8")))
     data_dict = json.loads(msg.payload)
+    cursor = db.cursor()
+    sql = """INSERT INTO malfunction (date,normal,defect)
+        SELECT current_date(),0,0
+        from dual
+        WHERE NOT EXISTS ( SELECT * FROM malfunction WHERE date = (%s))"""
+    cursor.execute(sql,today)
+    sql = """INSERT INTO hastrack (date,normal,defect)
+    SELECT current_date(),0,0
+    from dual
+    WHERE NOT EXISTS ( SELECT * FROM hastrack WHERE date = (%s))"""
+    cursor.execute(sql, today)
     TrackId = None
     if data_dict["Wrapper"][2]["value"] and trackflag:
         print("ok")
@@ -40,7 +51,7 @@ def on_message(client, userdata, msg):
             data_dict["Wrapper"][40]["value"], "%Y-%m-%dT%H:%M:%S.%fZ"
         )
         start_time = dataTime
-        end_time = start_time + timedelta(seconds=25)
+        end_time = start_time + timedelta(seconds=31)
         cursor = db.cursor()
         sql = "INSERT INTO track (start, end) VALUES (%s, %s)"
         cursor.execute(sql, (start_time, end_time))
@@ -50,8 +61,7 @@ def on_message(client, userdata, msg):
     Datetime = datetime.strptime(
         data_dict["Wrapper"][40]["value"], "%Y-%m-%dT%H:%M:%S.%fZ"
     )
-    sql = "SELECT * FROM track order by id desc limit 1"
-    cursor = db.cursor()
+    sql = "SELECT * FROM track order by id desc limit 1";
     cursor.execute(sql)
     r = cursor.fetchone()
     Start = data_dict["Wrapper"][0]["value"]
@@ -60,28 +70,33 @@ def on_message(client, userdata, msg):
     No3Motor1 = int(data_dict["Wrapper"][34]["value"])
     No3Motor2 = int(data_dict["Wrapper"][35]["value"])
     Dicevalue = int(data_dict["Wrapper"][38]["value"])
-    if r:
-        if Datetime >= r[1] and Datetime <= r[2]:
-            TrackId = r[0]
-    cursor = db.cursor()
-    print(
-        Datetime, Start, No1Action, No2InPoint, No3Motor1, No3Motor2, Dicevalue, TrackId
-    )
+    sql = """SELECT * FROM malfunction where date = (%s)"""
+    cursor.execute(sql, today)
+    row = cursor.fetchone()
+    if int(No3Motor2) > 0 and int(No3Motor2) <= 18000000 and int(No3Motor1) > 0 and int(No3Motor1) <= 1150000:
+        sql = """UPDATE malfunction set normal = (%s) where date = (%s)"""
+        cursor.execute(sql, (int(row[1]) + 1, row[0]))
+    elif int(No3Motor2) < 0 or int(No3Motor2) > 18000000 or int(No3Motor1) < 0 or int(No3Motor1) > 1150000:
+        sql = """UPDATE malfunction set detect = (%s) where date = (%s)"""
+        cursor.execute(sql,(int(row[2]) + 1, row[0]))
+    if Datetime >= r[1] and Datetime <= r[2]:
+        TrackId = r[0]
+    sql = """SELECT * FROM hastrack where date = (%s)"""
+    cursor.execute(sql,today)
+    row = cursor.fetchone()
+    print(row)
+    if not TrackId:
+        sql = """UPDATE hastrack set defect = (%s) where date = (%s)"""
+        cursor.execute(sql,(int(row[2]) + 1 , row[0]))
+    else:
+        sql = """UPDATE hastrack set normal = (%s) where date = (%s)"""
+        cursor.execute(sql,(int(row[1]) + 1, row[0]))
+    print(Datetime, Start, No1Action, No2InPoint, No3Motor1, No3Motor2, Dicevalue,TrackId)
     sql = """INSERT INTO record (Datetime,Start,No1Action,No2InPoint,No3Motor1,No3Motor2,Dicevalue,TrackId) values (%s,%s,%s,%s,%s,%s,%s,%s)"""
     cursor.execute(
-        sql,
-        (
-            Datetime,
-            Start,
-            No1Action,
-            No2InPoint,
-            No3Motor1,
-            No3Motor2,
-            Dicevalue,
-            TrackId,
-        ),
-    )
+        sql, (Datetime, Start, No1Action, No2InPoint, No3Motor1, No3Motor2, Dicevalue,TrackId))
     db.commit()
+
 
 
 # 새로운 클라이언트 생성
